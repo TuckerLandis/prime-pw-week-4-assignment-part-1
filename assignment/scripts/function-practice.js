5b6b0f80e85e2c27cef2d1020fd34a96c8cdfb9e
TuckerLandis <landistuckerc@gmail.com>
2021-04-20 17:08:28 -0500

    got #7, #8 & #9 working

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,75 +14,124 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, for example 'Hello, Jo!'
 function helloName( name ) {
-  return;
+  return 'Hello, ' + name;
 }
 // Remember to call the function to test
-
+console.log(helloName('Tucker'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+function addNumbers( firstNumber, secondNumber ) {
+  return (firstNumber + secondNumber);
 }
+console.log(addNumbers(2,3));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree( x, y, z ){
+  return ((x * y) * z);
 }
+console.log(multiplyThree(2,3,4));
 
 
-// 5. Function that will return true if a number is positive, 
+// 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
+  } else if ( number < 0) {
+    return false;
+  } else {
+    return false; // "if 0"
   }
-    return;
 }
-// Call the function to test each outcome (true & false) 
+// Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
 console.log( 'isPositive - should say false', isPositive(0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
 
 
-// 6. Function to return the _last_ item in an array. If the 
+// 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+  if (array) {
+  return array[array.length-1];
+} else {
+  return 'undefined';
+  }
 }
 
-// 7. Function to find a value in an array. Return true if the 
+let numbersList = [ 0, 7, 1, 4, 6 ];
+let emptyNumbersList = [ ];
+
+console.log(getLast(numbersList));
+console.log(getLast(emptyNumbersList));
+
+// 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
-//    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ){
-  
+//    DO NOT use Array.includes, Array.indexOf, or Array.find
+
+/*function find( value, array ){
+  let i = 0;
+  while ( i <= array.length ) {
+    if ( value === array[i] ) {
+      i++;
+      return true;
+    } else {
+      i++;
+      return false;
+    }
+  }
 }
+*/
+// curious why the above didn't work, i'll remove it if i figure it out on my own
+
+function find( value, array ) {
+  let i = 0;
+  for ( value of array ) {
+    if ( value === array[i] ) {
+      i++;
+      return true;
+    } else {
+      i++;
+      return false;
+    }
+  }
+}
+console.log(find( 7, numbersList ));
 
 // ----------------------
 // Stretch Goals
 // ----------------------
-// 8. Function to check if a letter is the first letter in a 
+// 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if ( letter === string[0] ) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
+function sumAll( array ) {
+  let sum = 0;
   // TODO: loop to add items
+  for (i = 0; i < array.length; ++i) {
+    sum += array[i];
+  }
   return sum;
 }
+console.log(sumAll(numbersList));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
 
-
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
-//     CodeWars(https://www.codewars.com/). Then describe it 
+// 11. Pick a problem from Edabit(https://edabit.com/) or
+//     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
